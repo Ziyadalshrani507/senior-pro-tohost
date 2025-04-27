3da0c430ed50534dc6e6ee2385ad7e6f487145c0
Ziyadalshrani507 <220110839@psu.edu.sa>
2025-04-28 01:19:40 +0300

    fix errors
    fixed some errors with the frontend in the destination file

---
+++ frontend/src/pages/DestinationDetails/DestinationDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DestinationDetails = () => {
+  return (
+    <div>
+      <h1>Destination Details</h1>
+      <p>Details about the selected destination will be displayed here.</p>
+    </div>
+  );
+};
+
+export default DestinationDetails;
